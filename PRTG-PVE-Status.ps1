3a638ca78790cd38b9da7f515a29bb6cf1e69640
Kevin <50058270+Brotpilot-921@users.noreply.github.com>
2025-04-27 20:18:05 +0200

    Update PRTG-PVE-Status.ps1
    Added filter to $all_vms because templates should not be counted as a virtual machine

--- PRTG-PVE-Status.ps1
+++ PRTG-PVE-Status.ps1
@@ -513,7 +513,7 @@ if ($channel_lxc) {
 if ($channel_vm) {
     $all_vms = $null
     $all_vms = Get-PveVm -PveTicket $ticket
-    $all_vms = $all_vms | Where-Object {$_.type -eq "qemu"}
+    $all_vms = $all_vms | Where-Object {($_.type -eq "qemu") -and ($_.template -eq "0")}
 
     #Node
     if ($ExcludeNode -ne "") {
